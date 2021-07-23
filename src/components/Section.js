fe4b2010140a7e6eac3f9c0429fc67c1d02c51b2
Utkarsh Patiyal <utkarshpatiyal99@gmail.com>
2021-07-23 21:43:44 +0530

    Body Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,21 +1,24 @@
 import React from 'react'
 import styled from 'styled-components';
 
-function Section() {
+function Section({ title , description , backgroundImg ,leftBtnText , rightBtnText }) {
   return (
-    <Wrap>
+    <Wrap bgImage={ backgroundImg }>
       <ItemText>
-        <h1>Model S</h1>
-        <p>Order Online for Touchless Delivery</p>
+        <h1>{ title }</h1>
+        <p>{ description }</p>
       </ItemText>
       <Buttons>
         <ButtonGroup>
           <LeftButton>
-            Custom Order
+            { leftBtnText }
           </LeftButton>
-          <RightButton>
-            Exisiting Inventory
+          { rightBtnText && 
+            <RightButton>
+            { rightBtnText }
           </RightButton>
+          }
+          
         </ButtonGroup>
         <DownArrow src="/images/down-arrow.svg"/>
         </Buttons>  
@@ -28,7 +31,7 @@ export default Section
 const Wrap = styled.div`
   width: 100vw;
   height: 100vh;
-  background-image: url('/images/model-s.jpg');
+  background-image: ${props => `url('/images/${props.bgImage}')`} ;
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
@@ -45,6 +48,9 @@ const ItemText = styled.div`
 const ButtonGroup = styled.div`
   display: flex;
   margin-bottom: 30px;
+  @media (max-width: 768px) {
+    flex-direction:column;
+  }
 `
 const LeftButton = styled.div`
   background-color: rgba(23,26,32,0.8);
@@ -64,12 +70,13 @@ const LeftButton = styled.div`
 
 const RightButton = styled(LeftButton)`
   background-color: white;
-  color: rgba(23,26,32,0.8);
-  opacity: 0.85;
+  color: black;
+  opacity: 0.65;
 `
 const DownArrow = styled.img`
-  margin-top: 20px;
+  
   height: 40px;
+  overflow-x: hidden;
   animation: animateDown infinite 1.5s;
 `
 const Buttons = styled.div`
